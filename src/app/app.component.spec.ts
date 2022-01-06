0427df68edadb8b803e8a13e6c2ac44d4e2b8959
stuppa <stupy4444caca@gmail.com>
2022-01-06 04:29:07 -0600

    Creando componente ui-informacion #3 con tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,32 @@
-import { TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let fixtureApp: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
       ],
     }).compileComponents();
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+  beforeEach(() => {
+    fixtureApp = TestBed.createComponent(AppComponent);
+    fixtureApp.detectChanges();
+  });
+
+  it('Debe instanciar el componente', () => {
+    const fixtureApp = TestBed.createComponent(AppComponent);
+    const app = fixtureApp.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'extra'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('extra');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('extra app is running!');
+  it('Debe tener contenido', () => {
+    const content = fixtureApp.debugElement.query(By.css('body')).nativeElement;
+    expect(content.innerHTML).not.toBeNull();
+    expect(content.innerHTML.length).toBeGreaterThan(0);
   });
 });
